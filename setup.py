312a8f0d12f3991a03218ac045cee71e3d9ef2e2
gergelyk <grzegorz.krason@gmail.com>
2016-10-19 01:23:44 +0200

    doc theme changed to classic

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='miscutils',
-    version='0.3',
+    version='0.4',
     description='Miscellaneous utilities for general use.',
     author='Grzegorz Krason',
     author_email='grzegorz@krason.biz',
     url='https://github.com/gergelyk/pymiscutils',
-    download_url='https://github.com/gergelyk/pymiscutils/tarball/0.3',
+    download_url='https://github.com/gergelyk/pymiscutils/tarball/0.4',
     license='MIT',
     packages=['miscutils'],
     keywords = ['iterating', 'head', 'tail'],

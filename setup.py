bd7ec706b72dfaeb79a86ca5ad895af37dcb2d5f
gergelyk <grzegorz.krason@gmail.com>
2016-11-07 22:12:55 +0100

    release 0.9

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='miscutils',
-    version='0.8',
+    version='0.9',
     description='Miscellaneous utilities for general use.',
     author='Grzegorz Krason',
     author_email='grzegorz@krason.biz',
     url='https://github.com/gergelyk/pymiscutils',
-    download_url='https://github.com/gergelyk/pymiscutils/tarball/0.8',
+    download_url='https://github.com/gergelyk/pymiscutils/tarball/0.9',
     license='MIT',
     packages=['miscutils'],
     keywords = ['iterating', 'head', 'tail', 'listing', 'router', 'proxy', 'prompt', 'shell'],

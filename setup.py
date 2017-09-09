a3eb222e1b430b250ac07cf69b4e8f237092fbd5
gergelyk <grzegorz@krason.me>
2017-09-09 21:07:09 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -15,9 +15,19 @@ setup(
     packages = ['miscutils'],
     package_data = {'': ['VERSION']},
     keywords = ['iterating', 'head', 'tail', 'listing', 'router', 'proxy', 'prompt', 'shell'],
-    classifiers = [],
     entry_points = {
-        'console_scripts': ['pylisting=miscutils.listing:main'],
+        'console_scripts': ['pylisting=miscutils.tools.experimental.listing:main'],
     },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
 )
 
